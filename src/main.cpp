9a8da8b337325d0978c19d5e070a5f86b9806c7d
Bryce512 <bryce.whitworth@gmail.com>
2025-01-27 11:41:22 -0700

    lights working

--- src/main.cpp
+++ src/main.cpp
@@ -16,14 +16,18 @@ void setup() {
   pinMode(Green, OUTPUT);
   pinMode(Blue, OUTPUT);
   pinMode(button, INPUT);
+
+  Serial.print("Setup complete");
+  // Serial.print(button);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if (button == HIGH) {
+  if (digitalRead(button) == HIGH) {
     loopCount++;
-    Serial.print("Button Pressed");
+    Serial.println("Button Pressed");
     changeLights();
+    delay(500);
   }
 }
 
@@ -31,32 +35,54 @@ void changeLights() {
   Serial.print("Loop Count: "); Serial.println(loopCount);
   if (loopCount == 1) {
     analogWrite(Red, 0);
+    analogWrite(Blue,255);
+    analogWrite(Green, 255);
   } else if (loopCount == 2 ) {
+    analogWrite(Red,255);
+    analogWrite(Blue,255);
     analogWrite(Green, 0);
   } else if (loopCount == 3) {
     analogWrite(Blue, 0);
+        analogWrite(Red,255);
+    analogWrite(Blue,0);
+    analogWrite(Green, 255);
   } else if (loopCount == 4) {
     gradualLights();
   } else if (loopCount == 5) {
     lightsOff();
     loopCount = 0;
-    Serial.print("Loop Count Reset");
+    Serial.println("Loop Count Reset");
   }
 }
 
 void gradualLights() {
-  for (int i = 255; i > 0; i--) {
-    analogWrite(Red, i);
-    delay(10);
-  }
-  for (int i = 255; i < 0; i--) {
-    analogWrite(Green, i);
-    delay(10);
-  }
-  for (int i = 255; i < 00; i--) {
-    analogWrite(Blue, i);
-    delay(10);
-  }
+    analogWrite(Red, 0);
+    analogWrite(Blue, 255);
+    analogWrite(Green, 255);
+    for (int i = 255; i >= 0; i--) { // Bring Green Up Mixing Red and Green
+      analogWrite(Green, i);
+      delay(10);
+    }
+    for(int i = 0; i <= 255; i++) { //Bring Red Down leaving just Green
+      analogWrite(Red, i);
+      delay(10);
+    }
+    for (int i = 255; i >= 0; i--) { //Bring Blue up leaving Blue/Green
+      analogWrite(Blue, i);
+      delay(10);
+    }
+    for (int i = 0; i <= 255; i++) { //Bring Green Down leaving Blue
+      analogWrite(Green, i);
+      delay(10);
+    }
+    for (int i = 255; i >= 0; i--) { //Bring Red up leaving Blue/Red
+      analogWrite(Red, i);
+      delay(10);
+    }
+    for (int i = 0; i <= 255; i++) { //Bring Blue Down leaving Red
+      analogWrite(Blue, i);
+      delay(10);
+    }
 }
 
 void lightsOff(){
